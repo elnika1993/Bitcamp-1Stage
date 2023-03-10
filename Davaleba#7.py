837566aee9a33efcd624ee97705c488171532e56
elnika1993 <shubladze.nikoloz@yahoo.com>
2023-03-10 20:01:39 +0400

    Deep Thought

---
+++ Davaleba#7.py
@@ -0,0 +1,12 @@
+print("What is the asnwer to the Great Question of Life, the Universe and Everything ?")
+answer = input()
+if answer == "42" or answer =="forty-two" or answer =="forty two":
+    print("YES")
+else:
+    print("NO")
+
+    #match answer:
+    #case "42" | "forty two" | "forty-two":
+    #    print("YES")
+    #case _:
+    #    print("NO")
