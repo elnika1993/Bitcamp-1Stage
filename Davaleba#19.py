bc265d466d39f5369f77130a8b6d557e148da9da
elnika1993 <shubladze.nikoloz@yahoo.com>
2023-03-17 20:11:16 +0400

    Felipes Taqueria

---
+++ Davaleba#19.py
@@ -0,0 +1,20 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total = round(0,2)
+
+while True:
+    item = menu[input("Item: ")]
+    total += item
+    print("Total: $%.2f" %total)
+
+    
